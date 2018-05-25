07d967e9a045e563c82571cc3f73c12b9e5bf765
Stephen Cioffi <stephen@stephencioffi.com>
2018-05-25 17:23:27 -0400

    Updated copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<h5>Copyright &copy; {new Date().getFullYear()} Intelligent Self-Adaptive Simulation System</h5>
+		<h5>Copyright &copy; {new Date().getFullYear()} <a href="https://www.se.rit.edu" target="_blank">Department of Software Engineering</a> at the <a href="https://www.rit.edu" target="_blank">Rochester Institute of Technology</a></h5>
 	);
 }
 
