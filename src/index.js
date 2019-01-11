4a8f7fff2affdfd01082b2b7b45e03648fedb33e
Stephen Cioffi <stephen@stephencioffi.com>
2019-01-11 01:39:38 -0500

    Changed port for IS3 server.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 window.is3 = {
-	tacsim: process.env.NODE_ENV === "production" ? "https://api.is3tool.com:9000/tacsim" : "http://192.168.1.3:9000/tacsim"
+	tacsim: process.env.NODE_ENV === "production" ? "https://api.is3tool.com:8443/tacsim" : "http://192.168.1.3:8443/tacsim"
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
